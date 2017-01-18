e2ce87ba24c22423c55d5d0a7ca6fd33d3c368c9
Radoslav Georgiev <rgeorgiev583@gmail.com>
2017-01-18 20:01:32 +0200

    added naive solution of the knapsack problem

---
+++ 4-naive.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <vector>
+
+struct Item
+{
+    long long Weight, Value;
+};
+
+static long long knapsack(long long maxWeight, const std::vector<Item>& items)
+{
+    if (items.size() == 0 || maxWeight == 0)
+        return 0;
+
+    auto lastItem = items.back();
+    auto newItems = items;
+    newItems.pop_back();
+
+    if (lastItem.Weight > maxWeight)
+        return knapsack(maxWeight, newItems);
+
+    return std::max(lastItem.Value + knapsack(maxWeight - lastItem.Weight, newItems), knapsack(maxWeight, newItems));
+}
+
+int main()
+{
+    long long m;
+    scanf("%d", &m);
+    int n;
+    scanf("%d", &n);
+
+    if (m < 0 || n < 0)
+        return 1;
+
+    std::vector<Item> items(n);
+
+    for (auto& item: items)
+    {
+        scanf("%d", &item.Value);
+        scanf("%d", &item.Weight);
+    }
+
+    printf("%d\n", knapsack(m, items));
+    return 0;
+}
